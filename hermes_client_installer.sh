045dd1110e00deb5dcdaf553415976ad25787a37
Dev U. <matrix.h225@gmail.com>
2026-08-09 15:24:18 +0530

    auto add host

--- hermes_client_installer.sh
+++ hermes_client_installer.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 echo "Installing dependencies for debian/ubuntu"
+echo "Will ask for password to install packages"
+
+sudo apt install -y libsodium-dev libsqlcipher-dev libncurses-dev libreadline-dev tar
 
 echo "Installing Hermes client"
 curl -L -O https://github.com/karlson1337/hermes-messenger/releases/download/v1.0.0/hermes_client-linux-x86_64.tar.gz
@@ -10,6 +13,6 @@ rm -f hermes_client-linux-x86_64.tar.gz
 curl -L -O https://raw.githubusercontent.com/karlson1337/hermes-messenger/aws_host_scripts_depreciated/server_pk.key
 mkdir -p ~/.config/hermes/
 mv server_pk.key ~/.config/hermes/
+echo "hermes.karlson1337.me 8080" > ~/.config/hermes/host
 
-echo "Enter 'hermes.karlson1337.me as host and 8080 as port'"
 ./hermes_client
