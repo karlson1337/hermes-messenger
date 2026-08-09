57e1319c22ef1c2e196382a48a16962e52ac7c8f
Dev U. <matrix.h225@gmail.com>
2026-08-09 14:48:26 +0530

    added pk of hermes running on personal ec2

--- aws_run.sh
+++ aws_run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-if [ -d "hermes-messenger/bin/server" ]; then
+if [ -d "hermes-messenger/bin/server/" ]; then
   cd hermes-messenger/bin/server
   echo 8080 > port
   killall hermes_server
   ./hermes_server &
 else
-  git clone -b depreciated --single-branch https://github.com/karlson1337/hermes-messenger && cd hermes-messenger
+  git clone -b depreciated --single-branch https://github.com/karlson1337/hermes-messenger
+  cd hermes-messenger
+  make clean
   make
   cd bin/server
   echo 8080 > port
