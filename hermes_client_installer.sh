6a9ce78f597f7f5c146abe0d3a92984245f9ed6a
Dev U. <matrix.h225@gmail.com>
2026-08-09 15:16:35 +0530

    fix

--- hermes_client_installer.sh
+++ hermes_client_installer.sh
@@ -7,10 +7,9 @@ curl -L -O https://github.com/karlson1337/hermes-messenger/releases/download/v1.
 tar -xvf hermes_client-linux-x86_64.tar.gz
 rm -f hermes_client-linux-x86_64.tar.gz
 
-curl -O https://raw.githubusercontent.com/karlson1337/hermes-messenger/aws_host_scripts_depreciated/server_pk.key
+curl -L -O https://raw.githubusercontent.com/karlson1337/hermes-messenger/aws_host_scripts_depreciated/server_pk.key
 mkdir -p ~/.config/hermes/
-mv server_pk ~/.config/hermes/
+mv server_pk.key ~/.config/hermes/
 
-echo "Enter 'hermes.karlson1337.me as ip and 8080 as port'"
-killall hermes_client 
+echo "Enter 'hermes.karlson1337.me as host and 8080 as port'"
 ./hermes_client
